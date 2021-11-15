e56a9a6b2a2cbbf6ed230510029536e1e266c617
Shapka Pavel <shapka.pa@phystech.edu>
2021-11-15 22:10:24 +0300

    comment

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ import matplotlib.ticker as ticker
 from textwrap import wrap
 from collections import namedtuple
 
-SoundDataset = namedtuple('SoundDatset', ['mu', 'Cp', 'Cv'])
+SoundDataset = namedtuple('SoundDatset', ['mu', 'Cp', 'Cv']) # создание словаря с табличными значениями
 
 air = SoundDataset(28.97e-3, 1.0036, 0.7166)
 water = SoundDataset(18.01e-3, 1.863, 1.403)
@@ -13,23 +13,23 @@ co2 = SoundDataset(44.01e-3, 0.838, 0.649)
 
 R = 8.314
 
-with open('conditions.txt', 'r') as f:
+with open('conditions.txt', 'r') as f: # считывание измерений из файла
     text = f.readlines()
-    humity = float(re.search(r'\d+', text[0]).group(0))/100
+    humity = float(re.search(r'\d+\.\d+', text[0]).group(0))/100
     Pn = 3170
     temperature = float(re.search(r'\d+\.\d+', text[1]).group(0)) + 273
     xH2O = Pn * humity * water.mu / (R * temperature)
 
-xCO2 = np.linspace(0, 5, 100) / 100
-xAir = 1 - xCO2 - xH2O
+xCO2 = np.linspace(0, 5, 100) / 100 # массив с массовыми долями углекислого газа
+xAir = 1 - xCO2 - xH2O # массив с массовыми долями воздуха
 
 Cp = air.mu * air.Cp * xAir + water.mu * water.Cp * xH2O + co2.mu * co2.Cp * xCO2
 Cv = air.mu * air.Cv * xAir + water.mu * water.Cv * xH2O + co2.mu * co2.Cv * xCO2
 
-gamma = Cp / Cv
-mu = air.mu * xAir + water.mu * xH2O + co2.mu * xCO2
+gamma = Cp / Cv # массив с показателями адиабаты
+mu = air.mu * xAir + water.mu * xH2O + co2.mu * xCO2 # массив с показателями мю
 
-s_speed = np.sqrt(gamma * R * temperature / mu)
+s_speed = np.sqrt(gamma * R * temperature / mu) # массив со скоростями звука
 
 # for x in xCO2_array:
 #
@@ -56,9 +56,10 @@ lenth = 1158 * 10**(-3) # meters
 speed_air = lenth / delta_t_air # m/s
 speed_breath = lenth / delta_t_breath # m/s
 
-n_air = (speed_air - p[1])/p[0]
-n_breath = (speed_breath - p[1])/p[0]
+n_air = (speed_air - p[1])/p[0] # концентрация углекислого газа для измеренного значения скорости воздуха
+n_breath = (speed_breath - p[1])/p[0] # концентрация углекислого газа для измеренного значения скорости углекислого газа
 
+# Построение графика
 fig, ax = plt.subplots(figsize=(13, 10), dpi=75)
 plt.plot(xCO2 * 100, s_speed, color='b', marker='o', linestyle='-',  linewidth=0.5, markersize=1.5)
 
@@ -89,6 +90,9 @@ ax.grid(which='major', linewidth='1.25', linestyle='-', color='grey')
 ax.scatter(n_air * 100, speed_air, color='red', marker='+', s=150, linewidth=2)
 ax.scatter(n_breath * 100, speed_breath, color='green', marker='+', s=150, linewidths=2)
 
-plt.legend(['Аналитическая зависимость', 'Значение в воздухе: {:.2f} [м/с], {:.2f} [%]'.format(speed_air, n_air),
-            'Значения в выдохе: {:.2f} [м/с], {:.2f} [%]'.format(speed_breath, n_breath)], loc='upper right')
+plt.legend(['Аналитическая зависимость', 'Значение в воздухе: {:.2f} [м/с], {:.2f} [%]'.format(speed_air, n_air * 100),
+            'Значения в выдохе: {:.2f} [м/с], {:.2f} [%]'.format(speed_breath, n_breath * 100)], loc='upper right')
+
+plt.savefig('soundspeed.svg')
+
 plt.show()
